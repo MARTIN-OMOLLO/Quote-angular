fdf1039ad2d1f65ca5a5afbd525b6852a3bb3458
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-06 19:58:16 +0300

    add goal

--- src/app/modules/goals/goals.module.ts
+++ src/app/modules/goals/goals.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GoalsRoutingModule } from './goals-routing.module';
-import { AddGoalsComponent } from './components/add-goals/add-goals.component';
+// import { AddGoalsComponent } from './components/add-goals/add-goals.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { GoalComponent } from './goal/goal.component';
