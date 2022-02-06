b8b6f94deb2b2769dfeb9ad0d52d6568e3484e77
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-02-06 20:04:36 +0300

    form description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalsModule } from './modules/goals/goals.module';
+// import { GoalsModule } from './modules/goals/goals.module';
 
 @NgModule({
   declarations: [
